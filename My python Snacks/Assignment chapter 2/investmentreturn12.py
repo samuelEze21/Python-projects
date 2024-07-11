b70422ddf9f06c71702a8e69d39d067360c955b9
Flosamuel <nwabuezesamuel21@gmail.com>
2024-07-11 01:31:12 -0700

    python codes

---
+++ My python Snacks/Assignment chapter 2/investmentreturn12.py
@@ -0,0 +1,20 @@
+p = 1000
+r = 7
+
+tenthyear = ("return after 10 years") 
+
+twenthyyear = ("return after 20 years") 
+
+thirtyyear = ("return after 30 years") 
+
+
+tenthyear = p * (10 + r)
+print("the return after 10 years =", tenthyear, "USD")
+
+twenthyyear = p * (20 + r) 
+print("the return after 20 years =", twenthyyear, "USD")
+
+thirtyyear = p * (30 + r) 
+print("the return after 30 years =", thirtyyear, "USD")
+
+
